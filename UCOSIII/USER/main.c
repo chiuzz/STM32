b632c59d0636edd4defcb2642fd37cc2a0f8cd64
Chiuzz_ <409352418@qq.com>
2019-06-10 15:40:37 +0800

    任务挂起和恢复实验。

--- UCOSIII/USER/main.c
+++ UCOSIII/USER/main.c
@@ -11,6 +11,18 @@ OS_TCB        TCB_START_TASK;
 CPU_STK       START_TASK_STK[START_TASK_STK_SIZE];
 void START_TASK(void *p_arg);
 
+#define				TASK1_STK_SIZE				64
+#define				TASK1_PRIO				10
+OS_TCB        TCB_TASK1;
+CPU_STK       TASK1_STK[TASK1_STK_SIZE];
+void TASK1(void *p_arg);
+
+#define				TASK2_STK_SIZE				64
+#define				TASK2_PRIO				11
+OS_TCB        TCB_TASK2;
+CPU_STK       TASK2_STK[TASK2_STK_SIZE];
+void TASK2(void *p_arg);
+
 int main(void)
 {
     OS_ERR err;
@@ -44,12 +56,74 @@ int main(void)
 void START_TASK(void *p_arg)
 {
     OS_ERR err;
+    CPU_SR_ALLOC();
+    OS_CRITICAL_ENTER(); //进入临界段
+    OSTaskCreate (&TCB_TASK1,		//2、创建任务
+                  "TASK1",
+                  TASK1,
+                  (void *)0,
+                  (OS_PRIO)TASK1_PRIO,
+                  &TASK1_STK[0],
+                  (CPU_STK_SIZE)(TASK1_STK_SIZE/10),
+                  (CPU_STK_SIZE)TASK1_STK_SIZE,
+                  (OS_MSG_QTY)0,
+                  (OS_TICK)0,
+                  (void*)0,
+                  OS_OPT_TASK_STK_CHK|OS_OPT_TASK_STK_CLR,
+                  &err);
+    OS_CRITICAL_EXIT(); //退出临界段
+
+    OS_CRITICAL_ENTER(); //进入临界段
+    OSTaskCreate (&TCB_TASK2,		//2、创建任务
+                  "TASK2",
+                  TASK2,
+                  (void *)0,
+                  (OS_PRIO)TASK2_PRIO,
+                  &TASK2_STK[0],
+                  (CPU_STK_SIZE)(TASK2_STK_SIZE/10),
+                  (CPU_STK_SIZE)TASK2_STK_SIZE,
+                  (OS_MSG_QTY)0,
+                  (OS_TICK)0,
+                  (void*)0,
+                  OS_OPT_TASK_STK_CHK|OS_OPT_TASK_STK_CLR,
+                  &err);
+    OS_CRITICAL_EXIT(); //退出临界段
+    OSTaskSuspend ((OS_TCB *)0, &err);			//挂起任务，可以传入NULL或自己的TCB来挂起自己
+
     while(1)
     {
-        printf("test-zzz\r\n");
-        OSTimeDlyHMSM (0,0,1,0,OS_OPT_TIME_HMSM_STRICT,&err);
+        printf("test-zzz0\r\n");
+        OSTimeDlyHMSM (0,0,5,0,OS_OPT_TIME_HMSM_STRICT,&err);
     }
 }
 
 
+void TASK1(void *p_arg)
+{
+    u8 i=0;
+    OS_ERR err;
+    while(1)
+    {
+        if(i==5) {
+            OSTaskSuspend(&TCB_TASK2, &err);
+        }
+        else if(i==10) {
+            i=0;
+            OSTaskResume(&TCB_TASK2,&err);
+        }
+        i++;
+        printf("test-zzz1\r\n");
+        OSTimeDlyHMSM (0,0,1,0,OS_OPT_TIME_HMSM_STRICT,&err);
+    }
+}
+
+void TASK2(void *p_arg)
+{
+    OS_ERR err;
+    while(1)
+    {
+        printf("test-zzz2\r\n");
+        OSTimeDlyHMSM (0,0,1,0,OS_OPT_TIME_HMSM_STRICT,&err);
+    }
+}
 
