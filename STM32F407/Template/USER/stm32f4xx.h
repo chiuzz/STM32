67f44e9eefb7db5cea820fdc179ed29d1569bca6
Chiuzz_ <409352418@qq.com>
2019-07-01 13:46:55 +0800

    修改时钟配置。

--- STM32F407/Template/USER/stm32f4xx.h
+++ STM32F407/Template/USER/stm32f4xx.h
@@ -120,7 +120,7 @@ extern "C" {
   */
 
 #if !defined  (HSE_VALUE)
-#define HSE_VALUE    ((uint32_t)25000000) /*!< Value of the External oscillator in Hz */
+#define HSE_VALUE    ((uint32_t)8000000) /*!< Value of the External oscillator in Hz */
 
 #endif /* HSE_VALUE */
 
